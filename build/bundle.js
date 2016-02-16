30a6e63ea005eb2a6876dbe080ce4ed92f27c37f
Luke Davis <luke@peopleofthebit.com>
2016-02-16 14:39:43 -0800

    close #4

--- build/bundle.js
+++ build/bundle.js
@@ -1778,7 +1778,166 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var darwa = __webpack_require__(4);
-
+	     
+	zi={};
+	zi.config=function(){
+	  var grey=davis.randomColor("gray");
+	  var margin=(_.random(5,10))+"px";
+	  
+	  var css={
+	    "body":{
+	      "margin":"0",
+	      "padding":"0",
+	      "background":"#ccc",
+	      "font-size":app.m.fontSize+"em",
+	      "font-family":_.sample(["arial","times","garamond","verdana"])
+	    },
+	    "h2":{
+	      "margin-bottom":"0",
+	      "padding-bottom":"0"
+	    },
+	    "#navigation":{
+	      "position":"fixed",
+	      "height":"100%",
+	      "top":"0px",
+	      "left":"0px",
+	      "background":"#000",
+	      "opacity":"0.7",
+	      "overflow":"hidden"
+	    },
+	    "#navigation span":{
+	      "padding":"15px",
+	      "display":"block",
+	      "text-align":"center",
+	      "background":"#fff",
+	      "border":"1px solid #000",
+	      "cursor":"pointer",
+	      "font-size":"20px",
+	      "font-weight":"bold",
+	      "font-family":"courier"
+	    },
+	    "#navigation span.ancillary-text":{
+	      "display":"inline",
+	      "display":"none",
+	      "border":"0",
+	      "padding":"0",
+	      "margin":"0"
+	    },
+	    "table td":{
+	      "vertical-align":"top"
+	    },
+	    "td.col-2":{
+	      "width":"50%"
+	    },
+	    "table":{
+	      "width":"100%",
+	      "border-spacing":margin,
+	      "border-collapse":"separate"
+	    },
+	    "#contact-information":{
+	      "padding-top":margin,
+	      "padding-bottom":margin,
+	      "font-size":(0.1*_.random(7,9))+"em"
+	    },
+	    "#name":{
+	      "font-size":(0.1*_.random(20,50))+"em"
+	    },
+	    ".section-title":{
+	      "font-size":(0.1*_.random(8,20))+"em"
+	    },
+	    ".section":{
+	    },
+	    ".screen-wrapper":{
+	      "background":"#fff"
+	    },
+	    ".position-period":{
+	      "float":"right"
+	    },
+	    ".position-metadata":{
+	    },
+	    ".position":{
+	      "margin-top":margin,
+	      "margin-bottom":margin,
+	    },
+	    ".project":{
+	      "margin-top":margin,
+	      "margin-bottom":margin,
+	    },
+	    ".project-title":{
+	    },
+	    ".project-notes":{
+	    },
+	    ".section":{
+	      "margin-bottom":margin
+	    },
+	    ".collection":{
+	      "margin-top":margin,
+	      "margin-bottom":margin
+	    },
+	    "#personal-note":{
+	      "margin-top":margin,
+	      "margin-bottom":margin,
+	    },
+	    "input[type=text]":{
+	      "width":"100%"
+	    },
+	    "#save-data":{
+	      "cursor":"pointer",
+	      "display":"block",
+	      "margin-top":"20px",
+	      "background":"#333",
+	      "color":"#fff"
+	    },
+	    ".user-position-input-area, .user-project-input-area":{
+	      "margin-bottom":"20px"
+	    }
+	  };
+	  
+	  davis.maybe(app.m.genome[17],function(){
+	    css["#name"]["color"]="#fff";
+	    css["#name"]["background"]=grey;
+	    css["#name"]["text-align"]="center";
+	    css["#name"]["padding"]=(_.random(20,50) )+"px";
+	  });
+	  
+	  davis.maybe(app.m.genome[18],function(){
+	    css[".position-metadata"]["font-weight"]="bold";
+	    css[".project-title"]["font-weight"]="bold";
+	    css[".project-notes"]["font-weight"]="bold";
+	  },function(){
+	    css[".position-metadata"]["color"]=grey;
+	    css[".project-title"]["color"]=grey;
+	    css[".project-notes"]["color"]=grey;
+	  });
+	  
+	  davis.maybe(app.m.genome[19],function(){
+	    css[".section"]["border-bottom"]=(_.random(0,5)+"px solid "+grey);
+	  },function(){
+	    css[".section-title"]["color"]="#fff";
+	    css[".section-title"]["background"]=grey;
+	    css[".section-title"]["padding"]=(_.random(5,10))+"px";
+	  });
+	  
+	  return css;
+	};
+	zi.transform=function(css){
+	    var c="";
+	    for (var selector in css){
+	        c+=selector+"{";
+	        for (var property in css[selector]){
+	            c+=property+" : "+css[selector][property]+";";
+	        }
+	        c+="}";
+	    }
+	    return c;
+	};
+	zi.css=function(){
+	    if ($("head style#zi").length<1){
+	        $("head").append("<style type='text/css' id='zi'></style>");
+	    }
+	    $("head style#zi").html( this.transform( this.config() ) );
+	};
+	        
 	var style = function(genome){
 	  var auto1 = {
 	    'background-color': '#fff',
